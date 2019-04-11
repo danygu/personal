bf1e2381ce9b94d48822f2932064ed7cabc81e58
Daniela Gutierrez <danygtz11.97@gmail.com>
2019-04-11 09:18:19 -0500

    2

--- js/web.js
+++ js/web.js
@@ -1,6 +1,50 @@
-$(function() {
-    $('a[href*=#]').on('click', function(e) {
-      e.preventDefault();
-      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-    });
+/* Burger menu */
+const menuToggler = document.querySelector(".menu-toggler");
+const menuMenu = document.querySelector(".menu ul");
+const menuLinks = document.querySelectorAll(".menu a");
+
+menuToggler.addEventListener("click", menuTogglerClick);
+
+function menuTogglerClick() {
+  menuToggler.classList.toggle("open-menu-toggler");
+  menuMenu.classList.toggle("open");
+}
+
+menuLinks.forEach(elem => elem.addEventListener("click", menuLinkClick));
+
+function menuLinkClick() {
+  if(menuMenu.classList.contains("open")) {
+    menuToggler.click();
+  }
+}
+/* END BURGER MENU */
+
+
+
+/*TEXT ANIMATION*/
+function autoType(elementClass, typingSpeed){
+  var thhis = $(elementClass);
+  thhis.css({
+    "position": "relative",
+    "display": "inline-block"
   });
+  thhis.prepend('<div class="cursor" style="right: initial; left:0;"></div>');
+  thhis = thhis.find(".text-js");
+  var text = thhis.text().trim().split('');
+  var amntOfChars = text.length;
+  var newString = "";
+  thhis.text("|");
+  setTimeout(function(){
+    thhis.css("opacity",1);
+    thhis.prev().removeAttr("style");
+    thhis.text("");
+    for(var i = 0; i < amntOfChars; i++){
+      (function(i,char){
+        setTimeout(function() {        
+          newString += char;
+          thhis.text(newString);
+        },i*typingSpeed);
+      })(i+1,text[i]);
+    }
+  },1500);
+}
