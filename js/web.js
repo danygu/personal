52eb80c3c898666c81b2b0b81c68e342d6b717a5
danygu <46565539+danygu@users.noreply.github.com>
2019-05-11 17:36:33 -0500

    galería

--- js/web.js
+++ js/web.js
@@ -1,24 +1,39 @@
-/* Burger menu */
-const menuToggler = document.querySelector(".menu-toggler");
-const menuMenu = document.querySelector(".menu ul");
-const menuLinks = document.querySelectorAll(".menu a");
+$(document).ready(function () {
+  $(document).on("scroll", onScroll);
+  
+  //smoothscroll
+  $('a[href^="#"]').on('click', function (e) {
+      e.preventDefault();
+      $(document).off("scroll");
+      
+      $('a').each(function () {
+          $(this).removeClass('active');
+      })
+      $(this).addClass('active');
+    
+      var target = this.hash,
+          menu = target;
+      $target = $(target);
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top+2
+      }, 500, 'swing', function () {
+          window.location.hash = target;
+          $(document).on("scroll", onScroll);
+      });
+  });
+});
 
-menuToggler.addEventListener("click", menuTogglerClick);
-
-function menuTogglerClick() {
-  menuToggler.classList.toggle("open-menu-toggler");
-  menuMenu.classList.toggle("open");
+function onScroll(event){
+  var scrollPos = $(document).scrollTop();
+  $('.navbar a').each(function () {
+      var currLink = $(this);
+      var refElement = $(currLink.attr("href"));
+      if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+          $('.navbar ul li a').removeClass("active");
+          currLink.addClass("active");
+      }
+      else{
+          currLink.removeClass("active");
+      }
+  });
 }
-
-menuLinks.forEach(elem => elem.addEventListener("click", menuLinkClick));
-
-function menuLinkClick() {
-  if(menuMenu.classList.contains("open")) {
-    menuToggler.click();
-  }
-}
-/* END BURGER MENU */
-
-
-
-/*TEXT ANIMATION*/
